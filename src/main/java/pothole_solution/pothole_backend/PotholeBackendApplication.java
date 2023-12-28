6764fdf2e1d67b7030e4d3d186d98d97cb1776df
lcj1204 <dlckdwo1204@naver.com>
2023-12-28 20:32:56 +0900

    init commit

---
+++ src/main/java/pothole_solution/pothole_backend/PotholeBackendApplication.java
@@ -0,0 +1,13 @@
+package pothole_solution.pothole_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotholeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotholeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pothole_solution/pothole_backend/PotholeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pothole_solution.pothole_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotholeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
