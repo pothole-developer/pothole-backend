52edea6f3cbc1d8d2be776fc358db5a6b330e1ed
jjunsik <junsik8236@gmail.com>
2024-04-08 16:19:06 +0900

    [Build] 멀티 모듈 구성
    관제 시스템 API, 시공사 API, 공통 로직을 구현할 Core 모듈로 구성

---
+++ pothole-controlsystem-api/src/main/java/pothole_solution/controlsystem/ControlSystemApplication.java
@@ -0,0 +1,11 @@
+package pothole_solution.controlsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ControlSystemApplication.class, args);
+    }
+}
