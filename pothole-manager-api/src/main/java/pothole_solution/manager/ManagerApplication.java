ee9a897ec78862c1e24e56e2b3ae3b98b3e6aa71
jjunsik <junsik8236@gmail.com>
2024-04-15 21:39:19 +0900

    [Build] 관제 시스템 및 시공사 모듈 추가

---
+++ pothole-manager-api/src/main/java/pothole_solution/manager/ManagerApplication.java
@@ -0,0 +1,15 @@
+package pothole_solution.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan("pothole_solution.core")
+@EnableJpaRepositories("pothole_solution.core")
+@SpringBootApplication(scanBasePackages = {"pothole_solution.core", "pothole_solution.manager"})
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+}
