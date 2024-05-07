13179baf88022a16b2fb6b62b2bd35b52beb6586
jjunsik <junsik8236@gmail.com>
2024-05-08 00:49:52 +0900

    [Refactor] Listener 클래스명 변경, Slack 토큰 로딩 및 메시지 전송 기능 분리

---
+++ pothole-core/src/main/java/pothole_solution/core/util/alarm/slack/listener/BootTimeListener.java
@@ -0,0 +1,30 @@
+package pothole_solution.core.util.alarm.slack.listener;
+
+import com.slack.api.model.block.LayoutBlock;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import pothole_solution.core.util.alarm.slack.SlackMessageFormatter;
+import pothole_solution.core.util.alarm.slack.SlackService;
+
+import java.time.Duration;
+import java.util.List;
+
+import static pothole_solution.core.util.alarm.slack.constant.SlackConstant.*;
+
+public class BootTimeListener implements SpringApplicationRunListener {
+    @Override
+    public void ready(ConfigurableApplicationContext context, Duration timeTaken) {
+        SpringApplicationRunListener.super.ready(context, timeTaken);
+
+        SlackService slackService = context.getBean(SlackService.class);
+
+        long millis = timeTaken.toMillis();
+        String startupTime = String.valueOf(millis / 1000.0);
+
+        boolean isManagerServer = context.getEnvironment().matchesProfiles("manager-dev");
+        String serverName = isManagerServer ? MANAGER_SERVER : WORKER_SERVER;
+
+        List<LayoutBlock> layoutBlocks = new SlackMessageFormatter().buildBootMessageFormat(serverName, startupTime, true);
+        slackService.sendMessage(POTHOLE_SERVER_DEPLOY, POTHOLE_SERVER_DEPLOY_PREVIEW_MSG, layoutBlocks);
+    }
+}
