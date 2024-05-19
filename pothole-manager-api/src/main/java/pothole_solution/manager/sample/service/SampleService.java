989c30ebdbd94b423045c6efe0a9b734a2b1c6ae
jjunsik <junsik8236@gmail.com>
2024-05-19 22:30:35 +0900

    [Feat] Pothole 필터 기능 서비스 구현

--- pothole-manager-api/src/main/java/pothole_solution/manager/sample/service/SampleService.java
+++ pothole-manager-api/src/main/java/pothole_solution/manager/sample/service/SampleService.java
@@ -3,18 +3,19 @@ package pothole_solution.manager.sample.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pothole_solution.core.Pothole;
-import pothole_solution.core.PotholeRepository;
-import pothole_solution.core.Progress;
+import pothole_solution.core.pothole.Pothole;
+import pothole_solution.core.pothole.PotholeRepository;
+import pothole_solution.core.pothole.Progress;
 
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SampleService {
     private final PotholeRepository potholeRepository;
 
+    @Transactional
     public Pothole register(Pothole pothole) {
         return potholeRepository.save(pothole);
     }
@@ -27,14 +28,30 @@ public class SampleService {
         return potholeRepository.findAll();
     }
 
+    @Transactional
     public Pothole changePotholeProgress(Long id) {
         Pothole pothole = potholeRepository.findById(id).get();
-        pothole.changeProgress(Progress.IN_PROGRESS);
+        pothole.changeProgress(Progress.EMERGENCY_ONGOING);
 
         return pothole;
     }
 
     public void deletePothole(Long id) {
         potholeRepository.deleteById(id);
+    }
+
+    public List<Pothole> getFilteredPotholes(Integer minImportance, Integer maxImportance, String process) {
+        List<Pothole> potholes = potholeRepository.findAll();
+
+        int actualMinImportance = (minImportance == null) ? 0 : minImportance;
+        int actualMaxImportance = (maxImportance == null) ? 100 : maxImportance;
+
+        return potholes.stream()
+                .filter(pothole ->
+                        (actualMinImportance <= pothole.getImportance() && pothole.getImportance() <= actualMaxImportance)
+                        &&
+                        (process == null || pothole.getProgress().getValue().equals(process))
+                )
+                .toList();
     }
 }
