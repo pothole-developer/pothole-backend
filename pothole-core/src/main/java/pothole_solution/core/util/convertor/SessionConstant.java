0770a70e027b92236e6bff74b0df683f9c03a3ed
lcj1204 <dlckdwo1204@naver.com>
2024-05-31 14:22:15 +0900

    [Feat] Session Constant 정의

---
+++ pothole-core/src/main/java/pothole_solution/core/util/convertor/SessionConstant.java
@@ -0,0 +1,6 @@
+package pothole_solution.core.util.convertor;
+
+public class SessionConstant {
+    public static final String MANAGER_SESSION_KEY = "managerId";
+    public static final String WORKER_SESSION_KEY = "workerId";
+}
