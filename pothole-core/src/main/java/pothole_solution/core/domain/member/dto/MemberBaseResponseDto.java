8d7f94c006d31304fec51810916d8cc68ade5d21
lcj1204 <dlckdwo1204@naver.com>
2024-05-31 14:20:18 +0900

    [Feat] 응답 성공시 memberId 반환하는 기본 Dto 구현

---
+++ pothole-core/src/main/java/pothole_solution/core/domain/member/dto/MemberBaseResponseDto.java
@@ -0,0 +1,18 @@
+package pothole_solution.core.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import pothole_solution.core.domain.member.entity.Member;
+
+@Getter
+@Builder
+public class MemberBaseResponseDto {
+
+    private Long memberId;
+
+    public static MemberBaseResponseDto of(Member member) {
+        return MemberBaseResponseDto.builder()
+                .memberId(member.getId())
+                .build();
+    }
+}
