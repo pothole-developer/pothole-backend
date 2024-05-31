32c671c40ca87a28c1aacbb795f48530253010c0
lcj1204 <dlckdwo1204@naver.com>
2024-05-31 14:37:38 +0900

    [Refactor] 불필요한 어노테이션 삭제

--- pothole-core/src/main/java/pothole_solution/core/infra/redis/RedisConfig.java
+++ pothole-core/src/main/java/pothole_solution/core/infra/redis/RedisConfig.java
@@ -10,11 +10,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-//@EnableRedisRepositories
-@EnableRedisHttpSession
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String redisHost;
