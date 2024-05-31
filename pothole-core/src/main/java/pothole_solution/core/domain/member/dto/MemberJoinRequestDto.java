d81259c9b4408ed3ddb5adc8b17249e99dbfe80b
lcj1204 <dlckdwo1204@naver.com>
2024-05-31 15:54:41 +0900

    [Feat] @valid 추가

--- pothole-core/src/main/java/pothole_solution/core/domain/member/dto/MemberJoinRequestDto.java
+++ pothole-core/src/main/java/pothole_solution/core/domain/member/dto/MemberJoinRequestDto.java
@@ -2,6 +2,7 @@ package pothole_solution.core.domain.member.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberJoinRequestDto {
     @NotBlank(message = "이름 입력은 필수 입니다.")
+    @Size(max = 50, message = "이름의 길이는 50글자 이하입니다.")
     private String name;
 
     @NotBlank(message = "이메일 입력은 필수 입니다.")
-    @Email
+    @Email(message = "이메일 형식이 아닙니다.")
     private String email;
 
     @NotBlank(message = "비밀번호 입력은 필수 입니다.")
@@ -23,6 +25,5 @@ public class MemberJoinRequestDto {
     private String contact;
 
     @NotBlank(message = "권한 입력은 필수 입니다.")
-    // annotation
     private String role;
 }
