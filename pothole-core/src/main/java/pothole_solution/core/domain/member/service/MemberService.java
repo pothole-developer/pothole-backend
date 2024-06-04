5fa142fb58e7f232c2d9d4250f3572eab37e9452
lcj1204 <dlckdwo1204@naver.com>
2024-06-04 20:46:18 +0900

    [Feat] 인증 관련 서비스 auth 디렉토리로 옮김

--- pothole-core/src/main/java/pothole_solution/core/domain/member/service/MemberService.java
+++ pothole-core/src/main/java/pothole_solution/core/domain/member/service/MemberService.java
@@ -1,9 +1,5 @@
 package pothole_solution.core.domain.member.service;
 
-import jakarta.servlet.http.HttpServletRequest;
-import pothole_solution.core.domain.member.dto.MemberJoinRequestDto;
-import pothole_solution.core.domain.member.dto.MemberLoginRequestDto;
-import pothole_solution.core.domain.member.dto.MemberBaseResponseDto;
 import pothole_solution.core.domain.member.entity.Member;
 
 public interface MemberService {
@@ -11,8 +7,4 @@ public interface MemberService {
     Member findByEmail(String email);
     boolean existsByEmail(String email);
     void validateDuplicateEmail(String email);
-    MemberBaseResponseDto join(MemberJoinRequestDto requestDto);
-    MemberBaseResponseDto login(MemberLoginRequestDto requestDto,
-                                HttpServletRequest httpServletRequest);
-    String logout(HttpServletRequest httpServletRequest);
 }
--- pothole-core/src/main/java/pothole_solution/core/util/convertor/SessionConstant.java
+++
@@ -1,6 +0,0 @@
-package pothole_solution.core.util.convertor;
-
-public class SessionConstant {
-    public static final String MANAGER_SESSION_KEY = "managerId";
-    public static final String WORKER_SESSION_KEY = "workerId";
-}
