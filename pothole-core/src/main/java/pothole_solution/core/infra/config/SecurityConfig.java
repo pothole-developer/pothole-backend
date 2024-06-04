00df70358fb0ee453cc93800ed1636d33fa769e6
lcj1204 <dlckdwo1204@naver.com>
2024-06-04 22:21:59 +0900

    [Feat] Spring Security config 구현

---
+++ pothole-core/src/main/java/pothole_solution/core/infra/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package pothole_solution.core.infra.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import pothole_solution.core.infra.config.filter.SessionAccessDeniedHandler;
+import pothole_solution.core.infra.config.filter.SessionAuthenticationEntryPoint;
+import pothole_solution.core.infra.config.filter.SessionAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final SessionAuthenticationFilter sessionAuthenticationFilter;
+    private final SessionAuthenticationEntryPoint sessionAuthenticationEntryPoint;
+    private final SessionAccessDeniedHandler sessionAccessDeniedHandler;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // 웹앱이기 때문에 불필요
+                .requestCache(RequestCacheConfigurer::disable) // 웹앱이기 때문에 불필
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .maximumSessions(2)
+                )
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                        .requestMatchers(
+                                AntPathRequestMatcher.antMatcher("/pothole/v1/auth/join"),
+                                AntPathRequestMatcher.antMatcher("/pothole/v1/auth/login")).permitAll()
+                        .requestMatchers(
+                                AntPathRequestMatcher.antMatcher("/pothole/v1/manager/**")).hasRole("MANAGER")
+                        .requestMatchers(
+                                AntPathRequestMatcher.antMatcher("/pothole/v1/worker/**")).hasRole("WORKER")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(sessionAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(sessionAuthenticationEntryPoint)
+                        .accessDeniedHandler(sessionAccessDeniedHandler)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
