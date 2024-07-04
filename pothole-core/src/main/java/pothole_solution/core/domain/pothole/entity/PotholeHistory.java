73a71a146021eb4afbc55aa253801ef14687e756
jjunsik <junsik8236@gmail.com>
2024-07-04 23:34:25 +0900

    [POT-56] [Feat] 포트홀의 진행 기록 Entity

---
+++ pothole-core/src/main/java/pothole_solution/core/domain/pothole/entity/PotholeHistory.java
@@ -0,0 +1,31 @@
+package pothole_solution.core.domain.pothole.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pothole_solution.core.domain.BaseTimeEntity;
+import pothole_solution.core.global.util.converter.ProgressEnumConverter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class PotholeHistory extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long potholeHistoryId;
+
+    @Column(nullable = false, length = 5)
+    @Convert(converter = ProgressEnumConverter.class)
+    private Progress processStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pothole_id")
+    private Pothole pothole;
+
+    @Builder
+    public PotholeHistory(Progress processStatus, Pothole pothole) {
+        this.processStatus = processStatus;
+        this.pothole = pothole;
+    }
+}
