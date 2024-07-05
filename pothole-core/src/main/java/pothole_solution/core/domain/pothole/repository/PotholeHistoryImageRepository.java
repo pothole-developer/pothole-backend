cd2924900398940ebb2108b8bfb5c6bf738a4e3d
jjunsik <junsik8236@gmail.com>
2024-07-05 17:37:21 +0900

    [POT-56] [Feat] PotholeHistoryImage Repository 구현

---
+++ pothole-core/src/main/java/pothole_solution/core/domain/pothole/repository/PotholeHistoryImageRepository.java
@@ -0,0 +1,29 @@
+package pothole_solution.core.domain.pothole.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pothole_solution.core.domain.pothole.entity.PotholeHistoryImage;
+
+import java.util.List;
+
+public interface PotholeHistoryImageRepository extends JpaRepository<PotholeHistoryImage, Long> {
+    @Query("select phi from PotholeHistoryImage phi " +
+            "where phi.potholeHistory.pothole.potholeId = :potholeId")
+    List<PotholeHistoryImage> findAllByPotholeId(@Param("potholeId") Long potholeId);
+
+    @Modifying
+    @Query("delete from PotholeHistoryImage phi " +
+            "where phi.potholeHistory.pothole.potholeId = :potholeId")
+    void deleteAllByPotholeId(@Param("potholeId") Long potholeId);
+
+    @Query("select phi from PotholeHistoryImage phi " +
+            "where phi.potholeHistory.potholeHistoryId = :potholeHistoryId")
+    List<PotholeHistoryImage> findAllByPotholeHistoryId(@Param("potholeHistoryId") Long potholeHistoryId);
+
+    @Modifying
+    @Query("delete from PotholeHistoryImage phi " +
+            "where phi.potholeHistory.potholeHistoryId = :potholeHistoryId")
+    void deleteAllByPotholeHistoryId(@Param("potholeHistoryId") Long potholeHistoryId);
+}
