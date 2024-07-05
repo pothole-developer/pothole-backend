c41073e441623132fe0288e44df5e70911e80cd5
jjunsik <junsik8236@gmail.com>
2024-07-05 17:39:53 +0900

    [POT-56] [Refactor] Pothole Service -> Interface 를 구현하도록 수정

--- pothole-manager-api/src/main/java/pothole_solution/manager/service/PotholeManagerService.java
+++ pothole-manager-api/src/main/java/pothole_solution/manager/service/PotholeManagerService.java
@@ -1,87 +1,22 @@
 package pothole_solution.manager.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import pothole_solution.core.domain.pothole.dto.PotholeFilterDto;
-import pothole_solution.core.domain.pothole.dto.request.PotholeChangeProcessStatusRequestDto;
+import pothole_solution.core.domain.pothole.dto.request.ChangePotholeProgressStatusRequestDto;
 import pothole_solution.core.domain.pothole.entity.Pothole;
-import pothole_solution.core.domain.pothole.repository.PotholeQueryDslRepository;
-import pothole_solution.core.domain.pothole.repository.PotholeRepository;
-import pothole_solution.core.infra.s3.ImageService;
 
 import java.util.List;
 
-import static pothole_solution.core.global.exception.CustomException.NOT_EXISTED_POTHOLE;
-import static pothole_solution.manager.service.constant.PotholeManagerServiceConstant.THUMBNAIL_DIR_NAME;
+public interface PotholeManagerService {
+    Pothole registerPothole(Pothole pothole, List<MultipartFile> potholeImg);
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class PotholeManagerService {
-    private final PotholeRepository potholeRepository;
-    private final PotholeQueryDslRepository potholeQueryDslRepository;
-    private final ImageService imageService;
+    Pothole getPothole(Long potholeId);
 
-    public Pothole register(Pothole pothole, MultipartFile potholeImg) {
-        if (potholeImg != null) {
-            String thumbnail = imageService.uploadImage(potholeImg, THUMBNAIL_DIR_NAME);
+    List<Pothole> getAllPotholes();
 
-            pothole.createThumbnailURL(thumbnail);
-        }
+    Pothole changePotholeProgressStatus(Long potholeId, ChangePotholeProgressStatusRequestDto changePotholeProcessStatusRequestDto);
 
-        return potholeRepository.save(pothole);
-    }
+    void deletePothole(Long potholeId);
 
-    @Transactional(readOnly = true)
-    public Pothole getPothole(Long id) {
-        return findById(id);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Pothole> getAllPotholes() {
-        return potholeRepository.findAll();
-    }
-
-    public Pothole changePotholeSimpleInfo(Long id, PotholeChangeProcessStatusRequestDto changeProcessStatusRequestDto, MultipartFile newImage) {
-        Pothole pothole = findById(id);
-
-        if (changeProcessStatusRequestDto != null) {
-            pothole.changeProgress(changeProcessStatusRequestDto.getProcessStatus());
-        }
-
-        if (newImage != null) {
-            String newImageUrl = imageService.updateImage(pothole.getThumbnail(), newImage, THUMBNAIL_DIR_NAME);
-
-            pothole.createThumbnailURL(newImageUrl);
-        }
-
-        return pothole;
-    }
-
-    public void deletePothole(Long id) {
-        Pothole pothole = findById(id);
-
-        imageService.deleteImage(pothole.getThumbnail());
-
-        potholeRepository.deleteById(id);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Pothole> getFilteredPotholes(PotholeFilterDto potholeFilterDto) {
-        Integer availableMinImportance = (potholeFilterDto.getMinImportance() == null) ? 0 : potholeFilterDto.getMinImportance();
-        Integer availableMaxImportance = (potholeFilterDto.getMaxImportance() == null) ? 100 : potholeFilterDto.getMaxImportance();
-
-        potholeFilterDto.changeToAvailableImportance(availableMinImportance, availableMaxImportance);
-
-        return potholeQueryDslRepository.findByFilter(potholeFilterDto);
-    }
-
-    private Pothole findById(Long id) {
-        return potholeRepository.findById(id)
-                .orElseThrow(
-                        () -> NOT_EXISTED_POTHOLE
-                );
-    }
+    List<Pothole> getFilteredPotholes(PotholeFilterDto potholeFilterDto);
 }
---
+++ pothole-manager-api/src/main/java/pothole_solution/manager/service/PotholeManagerServiceImpl.java
@@ -0,0 +1,149 @@
+package pothole_solution.manager.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import pothole_solution.core.domain.pothole.dto.PotholeFilterDto;
+import pothole_solution.core.domain.pothole.dto.request.ChangePotholeProgressStatusRequestDto;
+import pothole_solution.core.domain.pothole.entity.Pothole;
+import pothole_solution.core.domain.pothole.entity.PotholeHistory;
+import pothole_solution.core.domain.pothole.entity.PotholeHistoryImage;
+import pothole_solution.core.domain.pothole.repository.PotholeHistoryImageRepository;
+import pothole_solution.core.domain.pothole.repository.PotholeHistoryRepository;
+import pothole_solution.core.domain.pothole.repository.PotholeQueryDslRepository;
+import pothole_solution.core.domain.pothole.repository.PotholeRepository;
+import pothole_solution.core.infra.s3.ImageService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static pothole_solution.core.global.exception.CustomException.NONE_POTHOLE;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PotholeManagerServiceImpl implements PotholeManagerService {
+    private final PotholeRepository potholeRepository;
+    private final PotholeHistoryRepository potholeHistoryRepository;
+    private final PotholeHistoryImageRepository potholeHistoryImageRepository;
+    private final PotholeQueryDslRepository potholeQueryDslRepository;
+    private final ImageService imageService;
+
+    @Override
+    public Pothole registerPothole(Pothole pothole, List<MultipartFile> registerPotholeImages) {
+        List<String> imageUrls = imageService.uploadImages(registerPotholeImages, pothole.getProcessStatus().getValue());
+        pothole.createThumbnail(imageUrls.get(0));
+
+        // Pothole 저장
+        Pothole savedPothole = potholeRepository.save(pothole);
+
+        // PotholeHistory 생성 및 저장
+        PotholeHistory potholeHistory = new PotholeHistory(pothole.getProcessStatus(), savedPothole);
+        PotholeHistory savedPotholeHistory = potholeHistoryRepository.save(potholeHistory);
+
+        // PotholeHistoryImage 생성 및 저장
+        for (String imageUrl : imageUrls) {
+            PotholeHistoryImage potholeHistoryImage = new PotholeHistoryImage(imageUrl, savedPotholeHistory);
+            potholeHistoryImageRepository.save(potholeHistoryImage);
+        }
+
+        return savedPothole;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Pothole getPothole(Long potholeId) {
+        return findByPotholeId(potholeId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Pothole> getAllPotholes() {
+        return potholeRepository.findAll();
+    }
+
+    @Override
+    public Pothole changePotholeProgressStatus(Long potholeId, ChangePotholeProgressStatusRequestDto changePotholeProcessStatusRequestDto) {
+        Pothole pothole = findByPotholeId(potholeId);
+
+        List<PotholeHistory> potholeHistories = potholeHistoryRepository.findAllByPotholeId(potholeId);
+
+        pothole.changeProgress(changePotholeProcessStatusRequestDto.getProgressStatus());
+
+        // 해당 진행 상태에 대한 PotholeHistory 가 없다면 생성
+        createPotholeHistoryToProgressStatus(pothole, potholeHistories);
+
+        return pothole;
+    }
+
+    private void createPotholeHistoryToProgressStatus(Pothole pothole, List<PotholeHistory> potholeHistories) {
+        boolean isProcessStatusIncluded = potholeHistories.stream()
+                                                           .anyMatch(potholeHistory ->
+                                                                   potholeHistory.getProcessStatus().equals(pothole.getProcessStatus())
+                                                           );
+
+        if (!isProcessStatusIncluded) {
+            PotholeHistory potholeHistory = new PotholeHistory(pothole.getProcessStatus(), pothole);
+            potholeHistoryRepository.save(potholeHistory);
+        }
+    }
+
+    @Override
+    public void deletePothole(Long potholeId) {
+        Pothole pothole = findByPotholeId(potholeId);
+
+        String thumbnail = pothole.getThumbnail();
+
+        List<String> potholeHistoryImages = getPotholeHistoryImagesByPotholeId(potholeId);
+
+        // PotholeHistoryImage 삭제
+        potholeHistoryImageRepository.deleteAllByPotholeId(potholeId);
+
+        // PotholeHistory 삭제
+        potholeHistoryRepository.deleteAllByPotholeId(potholeId);
+
+        // Pothole 삭제
+        potholeRepository.deleteById(potholeId);
+
+        // S3 PotholeHistoryImage 삭제
+        if (!potholeHistoryImages.isEmpty()) {
+            potholeHistoryImages.forEach(imageService::deleteImage);
+        }
+
+        // S3 Pothole Thumbnail 삭제
+        if (thumbnail != null && !thumbnail.isEmpty()) {
+            imageService.deleteImage(thumbnail);
+        }
+    }
+
+    private List<String> getPotholeHistoryImagesByPotholeId(Long potholeId) {
+        List<PotholeHistoryImage> potholeHistoryImages = potholeHistoryImageRepository.findAllByPotholeId(potholeId);
+
+        List<String> potholeHistoryImagesUrl = new ArrayList<>();
+
+        if (potholeHistoryImages != null && !potholeHistoryImages.isEmpty()) {
+            potholeHistoryImages.forEach(potholeHistoryImage -> potholeHistoryImagesUrl.add(potholeHistoryImage.getImage()));
+        }
+
+        return potholeHistoryImagesUrl;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Pothole> getFilteredPotholes(PotholeFilterDto potholeFilterDto) {
+        Integer availableMinImportance = (potholeFilterDto.getMinImportance() == null) ? 0 : potholeFilterDto.getMinImportance();
+        Integer availableMaxImportance = (potholeFilterDto.getMaxImportance() == null) ? 100 : potholeFilterDto.getMaxImportance();
+
+        potholeFilterDto.changeToAvailableImportance(availableMinImportance, availableMaxImportance);
+
+        return potholeQueryDslRepository.findByFilter(potholeFilterDto);
+    }
+
+    private Pothole findByPotholeId(Long potholeId) {
+        return potholeRepository.findById(potholeId)
+                                .orElseThrow(
+                                        () -> NONE_POTHOLE
+                                );
+    }
+}
