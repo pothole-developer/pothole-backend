f919775e2166aa74848ddc5d7352a9a112bcd9f8
donghyuk <donghyuk454@gmail.com>
2024-10-19 16:56:49 +0900

    [POT-72] [Style] 주석 추가

--- pothole-core/src/main/java/pothole_solution/core/global/util/alarm/slack/constant/SlackConstant.java
+++ pothole-core/src/main/java/pothole_solution/core/global/util/alarm/slack/constant/SlackConstant.java
@@ -6,14 +6,15 @@ public class SlackConstant {
     // Slack에 생성된 채널명
     public static final String POTHOLE_SERVER_DEPLOY = "#포트홀";
 
+    // server name
     public static final String MANAGER_SERVER = "Manager";
-
     public static final String WORKER_SERVER = "Worker";
 
+    // preview message
     public static final String POTHOLE_SERVER_DEPLOY_PREVIEW_MSG = ":loudspeaker:  포트홀 서버가 배포되었습니다. 배포된 내용을 확인해주세요.";
     public static final String POTHOLE_DETECTION_START_PREVIEW_MSG = ":loudspeaker:  포트홀 탐지 차량이 출발하였습니다.";
 
+    // message info
     public static final String FAILURE_STARTUP_TIME = "0.000";
-
     public static final String NO_PR_INFO_MSG = "N/A";
 }
