828ef2ee8a3526be139d513ec88667d67c42c4cf
donghyuk <donghyuk454@gmail.com>
2024-10-19 16:08:22 +0900

    [POT-72] [Feat] 포트홀 차량 출발 시 호출할 api 개발

---
+++ pothole-manager-api/src/main/java/pothole_solution/manager/health/dto/RespHlthChkDto.java
@@ -0,0 +1,10 @@
+package pothole_solution.manager.health.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RespHlthChkDto {
+    private Boolean health;
+}
